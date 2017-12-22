0df1a5c0bf43485715b31375cc5b2fd1b7d750a6
王少鹏 <wangsp@many-it.com>
2017-12-22 12:41:35 +0800

    javassist proxy 单一接口实现

---
+++ src/test/java/javassist/ProxyTest.java
@@ -0,0 +1,30 @@
+package javassist;
+
+import com.hatox.common.javassist.Proxy;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProxyTest {
+    @Test
+    public void proxy_test(){
+        Proxy proxy=Proxy.getProxy(IDao.class);
+        IDao instance= (IDao) proxy.newInstance(new InvocationHandler() {
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if("getData".equals(method.getName())){
+                    assertEquals(args.length,0);
+                }else if("setData".equals(method.getName())){
+                    assertEquals(args[0],"name");
+                    assertEquals(args[1],"value");
+                }
+                return null;
+            }
+
+        });
+        instance.getData();
+        instance.setData("name","value");
+    }
+}
